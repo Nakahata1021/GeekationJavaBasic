f9c1a275717a59488846ef5379638529e3bdcf38
Nakahata1021 <j.nakahata.cingroup2012@gmail.com>
2024-12-25 11:55:05 +0900

    四則演算と計算処理課題修正その1

--- src/basicSyntax/Calculation.java
+++ src/basicSyntax/Calculation.java
@@ -6,26 +6,22 @@ public class Calculation {
 		// TODO 自動生成されたメソッド・スタブ
 		
 		// 1.の課題
-		int x = 5;
-	    int y = 10;
-	    int z = x + y;
-	    
-	    
-	    
-	    System.out.println("変数ｚの値は" + z + "です");
-	    
-	    //2.の課題
-	    double a = 5.25;
-	    a += 2.5;
-	    a *= 3.0;
-	    
-	    int b = (int) a;
-	    
-	    b += 10;
-	    
-	    System.out.println("変数aの値は" + a + "です");
-	    System.out.println("変数bの値は" + b + "です");
-	     
-	}
+        int x = 5;
+        int y = 10;
+        int z = x + y;
 
+        System.out.println("変数zの値は" + z + "です");
+
+        // 2.の課題
+        double a = 5.25;
+        a += 2.5;
+        a *= 3.0;
+
+        int b = (int) a;
+
+        b += 10;
+
+        System.out.println("変数aの値は" + a + "です");
+        System.out.println("変数bの値は" + b + "です");
+    }
 }
