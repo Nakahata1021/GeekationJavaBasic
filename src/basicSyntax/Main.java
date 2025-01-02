bac0d759c27fdc6d2088eb40275377759c921f44
Nakahata1021 <j.nakahata.cingroup2012@gmail.com>
2025-01-02 19:14:47 +0900

    メゾット課題

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,16 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int age = 30;
+		double height = 175.5;
+		GeekIntroduction.greeting(age, height);
+		
+		
+		int num1 = 21;
+		int num2 = 38;
+		GeekIntroduction.checkRange(num1, num2);
+		
+	}
+}
